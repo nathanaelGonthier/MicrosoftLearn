1b950fc4efba088865bf477a9f99287132979357
Nathanael Gonthier <gontthier.pro@gmail.com>
2022-04-07 20:43:07 +0400

    Complete

---
+++ SiteWebSimple/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+document.querySelector('.btn')
+.addEventListener('click',function(){
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+
+    if(className=="light-theme"){
+        this.textContent = "Dark";        
+    }
+    else{
+        this.textContent = "Light";        
+    }
+
+    console.log('current class name:'+className);
+});
